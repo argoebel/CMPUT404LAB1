e4bdd1141220563b78f9255d53e57c92dd598928
Austin Goebel <argoebel@ualberta.ca>
2020-01-06 17:48:33 -0700

    changed .text to .content

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
 import requests
 
 request = requests.get("http://google.com/")
-print(requests.get("https://raw.githubusercontent.com/argoebel/CMPUT404LAB1/master/script.py").text)
+print(requests.get("https://raw.githubusercontent.com/argoebel/CMPUT404LAB1/master/script.py").content)
